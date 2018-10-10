382d854265bdd7a9e6081eca98445e49e605b3f3
Emilien BERNARD <emilienbernard46@gmail.com>
2018-10-10 11:45:14 +0200

    feat(model): Initialisation du modèle

---
+++ app-hospital-model/src/main/java/fr/alm/app_hospital_model/App.java
@@ -0,0 +1,13 @@
+package fr.alm.app_hospital_model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
